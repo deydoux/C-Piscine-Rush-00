eab8193b54fcf57a03dda685e646a4994e3a748a
Timothe Perigaud <tperigau@z2r9p8.42lyon.fr>
2023-07-08 04:09:20 +0200

    new commit with all projects v2

---
+++ ex00/rush01.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 20:37:16 by deydoux           #+#    #+#             */
+/*   Updated: 2023/07/07 22:40:37 by tperigau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	line(char left, char middle, char right, int x)
+{
+	int	i;
+
+	i = 1;
+	while (i <= x)
+	{
+		if (i == 1)
+			ft_putchar(left);
+		else if (i == x)
+			ft_putchar(right);
+		else
+			ft_putchar(middle);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = y - 2;
+	if (x <= 0 || y <= 0)
+		return ;
+	line('/', '*', '\\', x);
+	while (i > 0)
+	{
+		line('*', ' ', '*', x);
+		i--;
+	}
+	if (y >= 2)
+		line('\\', '*', '/', x);
+}
